686297c80d4afa5e5fdbfa63477a91e48167c4cf
danielepo <dnl.pozzobon@gmail.com>
2017-02-23 19:45:35 +0100

    add generator tests

--- Tests/ExerciseTests/ProgramTests.cs
+++ Tests/ExerciseTests/ProgramTests.cs
@@ -1,16 +1,48 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Program;
 using Program.IOManagement;
 
 namespace Tests.ExerciseTests
 {
+    internal static class Extensions
+    {
+        public static string GetRow(this string input, int i)
+        {
+            return input.Split('\n')[i - 1];
+        }
+    }
     [TestFixture]
     public class ProgramTests
     {
         [Test]
         public void TestMethod1()
         {
-            var loader = new Loader();
-            Assert.That(loader.Load(), Is.EqualTo("content"));
+            var generator = new Generator();
+            var result = new Dictionary<CacheServer, List<Video>>();
+            Assert.That(generator.Convert(result).GetRow(1), Is.EqualTo("0"));
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            var generator = new Generator();
+            var result = new Dictionary<CacheServer, List<Video>>
+            {
+                [new CacheServer()] = new List<Video>()
+            };
+            Assert.That(generator.Convert(result).GetRow(1), Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            var generator = new Generator();
+            var result = new Dictionary<CacheServer, List<Video>>
+            {
+                [new CacheServer()] = new List<Video>(),
+                [new CacheServer()] = new List<Video>()
+            };
+            Assert.That(generator.Convert(result).GetRow(1), Is.EqualTo("2"));
         }
     }
 }
